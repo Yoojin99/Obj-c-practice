ad5939c470d71f444ec3c2e9d78e4c9997ad33bc
Yoojin99 <41438361+Yoojin99@users.noreply.github.com>
2021-03-30 11:30:36 +0900

    Initial Commit

---
+++ SampleC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SampleC
+//
+//  Created by NHN on 2021/03/30.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
